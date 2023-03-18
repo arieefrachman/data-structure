c1e04071eb923f39ff86088235b274e2a2355cb6
Arif Rakhaman <rachman.arief91@gmail.com>
2023-03-18 12:00:44 +0700

    initialize

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Rectangle {
+    int length;
+    int breath;
+};
+
+int main() {
+
+    return 0;
+}
+
+void arr(){
+    int n;
+    cout<<"Input a size:";
+    cin>>n;
+
+    int A[n];
+
+}
+
+void structure(){
+    struct Rectangle w{};
+
+}

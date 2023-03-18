add969b7ad091c505f645a1b391182487aaacad9
Arif Rakhaman <rachman.arief91@gmail.com>
2023-03-18 14:33:12 +0700

    feat: pointer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 
 struct Rectangle {
@@ -8,13 +10,6 @@ struct Rectangle {
 };
 
 int main() {
-    int a = 10;
-    int *p;
-
-    p = &a;
-
-    cout<<p;
-
     return 0;
 }
 
@@ -27,7 +22,50 @@ void arr(){
 
 }
 
-static void structure(){
+void structure(){
     struct Rectangle r1 = {10, 5};
     printf("%lu", sizeof(r1));
+}
+
+void pointer(){
+    int a = 10;
+    int *p;
+
+    p = &a;
+
+    //printf("using pointer: %d\n", *p);
+
+    int Arr[2] = {1,2};
+    int *ptr;
+
+    ptr = Arr;
+
+    // create memory allocation on heap with C style
+    int *c;
+    c = (int *)malloc(3 * sizeof(int));
+
+    // create memory allocation on heap with C++ style
+    int *d;
+    d = new int[3];
+
+    c[0] = 1; c[1] = 2; c[2] = 10;
+    d[0] = 1; d[1] = 2; d[2] = 10;
+
+    // free memory C style
+    free(c);
+
+    // free memory using C++ style
+    delete [ ] d;
+
+    int *p1;
+    char *p2;
+    float  *p3;
+    double *p4;
+    struct Rectangle *p5;
+
+    cout<<sizeof(p1)<<endl;
+    cout<<sizeof(p2)<<endl;
+    cout<<sizeof(p3)<<endl;
+    cout<<sizeof(p4)<<endl;
+
 }
